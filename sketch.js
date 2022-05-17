62855a76f081f5332fa2fd0d908b4b53223b6691
naoki <naokilamy@gmail.com>
2022-05-17 18:30:55 +0200

    word & victory testing

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ let numberOfLetters = 6;
 let numberOfTries = 6;
 let currentTry = 1;
 let letters = [];
+let word = "access";
+let gameStatus = "playing";
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,10 +18,40 @@ function setup() {
 }
 
 function draw() {
+  if(gameStatus == "playing"){
+    showgame();
+  }
+  checkStatus();
+}
+
+function showgame(){
+  push();
   background(220);
   fill(60);
   rect(width/3,0,width/3,height);//background
+  pop();
+
   Grid();
+}
+
+function checkStatus(){
+  if(gameStatus == "playing"){
+    if(currentTry>numberOfTries){
+      gameStatus = "gameover";
+    }
+    if(word == letters[currentTry-1].join("")){
+      gameStatus = "victory";
+    }
+  }else{
+    push();
+    fill(0);
+    strokeWeight(2);
+    stroke(60);
+    textSize(width/8);
+    rectMode(CENTER);
+    text(gameStatus,width/2,height/2,width/3,height/4)
+    pop();
+  }
 }
 
 function Grid(){
@@ -29,7 +62,7 @@ function Grid(){
   let h = w;
   for(let r = 0;r<numberOfTries;r++){
     for(let c = 0;c<numberOfLetters;c++){
-      push();
+      push();//important so that background color does not affect other letter
       fill(245,245,245,100);
       stroke(0);
       strokeWeight(3);
@@ -39,10 +72,17 @@ function Grid(){
       let squareH = (h/numberOfTries)-margin*2
       rect(squareX,squareY,squareW,squareH);//draw squares
       if(letters[r][c] != "empty"){
-        fill(255,0,255,200);
-        rect(squareX,squareY,squareW,squareH);
+        fill(255);
+        if(letters[r][c] == word.charAt(c)){//correct
+          fill(0,255,0,200);
+        }else if(word.includes(letters[r][c])){//is present at least once
+          fill(255,255,0,200);
+        }
+        rect(squareX,squareY,squareW,squareH);//draw background of square
+        rectMode(CENTER)
         textSize(squareW);
-        text(letters[r][c],squareX,squareY,squareW,squareH);//draw letters
+        fill(60);
+        text(letters[r][c],squareX+squareW/4*3,squareY+squareH/2,squareW,squareH);//draw letters
       }
       pop();
     }
@@ -51,7 +91,9 @@ function Grid(){
 
 function keyPressed(){
   console.log(letters);
-  typing();
+  if(gameStatus == "playing"){
+    typing();
+  }
 }
 
 function typing(){
@@ -68,6 +110,9 @@ function typing(){
 function checkWord(){
   if(letters[currentTry-1][numberOfLetters-1] != "empty"){
     currentTry++;
+    if(currentTry>numberOfTries){
+      gameStatus = "gameover";
+    }
   }
 }
   
