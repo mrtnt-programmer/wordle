01a0159479d07028348b6dfb6e42ab60ae7c4c71
Stephane Lamy <slamy@math.univ-toulouse.fr>
2022-05-18 16:04:07 +0200

    clean dict english and french

--- dict/filter.py
+++ dict/filter.py
@@ -1,7 +1,7 @@
 import unidecode
 
 for langue in ['francais', 'english']:
-  for nb_letters in [6,7,8]:
+  for nb_letters in [4,5,6,7,8]:
     for suffix in ['frequent', 'all']:
       i = 0
       filename = langue+'_'+suffix+'.txt'
@@ -16,7 +16,7 @@ for langue in ['francais', 'english']:
         for character in line: # remove special characters
           if character.isalnum():
             word += character
-        word = word.upper()
+        word = word.lower()
         if len(word) == nb_letters:
           words.add(word+'\n')
 
@@ -26,3 +26,16 @@ for langue in ['francais', 'english']:
       filename = langue+'_'+suffix+'_'+str(nb_letters)+'.txt'
       with open(filename, 'w') as f:
         f.writelines(words)
+    
+    #verify that frequent is a subset of all! 
+    filename = langue+'_frequent_'+str(nb_letters)+'.txt'
+    with open(filename, 'r') as f:
+      words_frequent = f.readlines()
+    filename = langue+'_all_'+str(nb_letters)+'.txt'
+    with open(filename, 'r') as f:
+      words_all = f.readlines()
+    for word in words_frequent:
+      if (word not in words_all):
+        print('Problem with', word, 'in', langue, nb_letters)
+    
+    print(langue, nb_letters, ': done')
