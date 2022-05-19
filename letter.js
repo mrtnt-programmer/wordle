e68f2b3501a526b93a37ce757bc7ae07c9562be0
naoki <naokilamy@gmail.com>
2022-05-19 22:12:41 +0200

    class done yellow still in progress

--- letter.js
+++ letter.js
@@ -1,8 +1,9 @@
 class Letter{
   constructor(r,c){
-    self.r = r;
-    self.c - c;
-    self.letter = "empty";
+    this.r = r;
+    this.c = c;
+    this.letter = "empty";
+    this.color = "gray";//gray green yellow
   }
 
 
--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup(){
       let newLetter = new Letter(r,c);
       line.push(newLetter);
     }
-    letters.push(line);
+    letter.push(line);
   }
   word = random(dict_frequent);
   print(word)
@@ -50,7 +50,7 @@ function checkStatus(){
       gameStatus = "gameover";
     }
     if(currentTry != 1){
-      if(word == letters[currentTry-2].join("")){
+      if(word == findWord(currentTry-2)){
         gameStatus = "victory";
       }
     }
@@ -85,15 +85,20 @@ function Grid(){
       rect(squareX,squareY,squareW,squareH);//draw squares
       if(letter[r][c].letter != "empty"){
         fill(255);
-        if(letter[r][c].letter == word.charAt(c) && currentTry-1>r){//correct
+        console.log(r,c,letter[r][c].color);
+        if(letter[r][c].color == "green"){//correct
           fill(0,255,0,200);//green
-        }else if(word.includes(letter[r][c].letter)&& currentTry-1>r){//test if maybe more than 1 copy of the letter
+        }else if(letter[r][c].color == "yellow"){
+          fill(255,255,0,200);//yellow
+        }
+        
+        /*else if(word.includes(letter[r][c].letter)&& currentTry-1>r){//test if maybe more than 1 copy of the letter
           let answerCopies = copies(word,letter[r][c].letter);
-          let tryCopies = copies(letter[r],letters[r][c]);//same but for the word tested
+          let tryCopies = copies(findWord(r),letter[r][c].letter);//same but for the word tested
           let yellow = false;
           let count;
           for(let l= 0;l<numberOfLetters;l++){
-            if(letters[r][l] == letters[r][c]){
+            if(letter[r][l].letter == letter[r][c].letter){
               count++;
             }
             if(count<l && c>=l){
@@ -103,17 +108,17 @@ function Grid(){
             if(count>answerCopies)break;         
           }
           console.log("yellow",answerCopies,tryCopies);
-//if(answerCopies>=tryCopies){
           if(yellow){
             console.log("more yellow");
             fill(255,255,0,200);//yellow
           }
         }
+        */
         rect(squareX,squareY,squareW,squareH);//draw background of square
         rectMode(CENTER)
         textSize(squareW);
         fill(60);
-        text(letters[r][c],squareX+squareW/4*3,squareY+squareH/2,squareW,squareH);//draw letters
+        text(letter[r][c].letter,squareX+squareW/4*3,squareY+squareH/2,squareW,squareH);//draw letters
       }
       pop();
     }
@@ -136,6 +141,27 @@ function copies(w, letter){//number of times a letter apears in the word w (give
   return c;
 }
 
+function checkWord(){
+  if(letter[currentTry-1][numberOfLetters-1].letter != "empty"){
+    for(let c=0;c<numberOfLetters;c++){
+      console.log("checkword");
+      if(letter[currentTry-1][c].letter == word.charAt(c)){
+        letter[currentTry-1][c].color = "green";
+      }
+      console.log("checkword",letter[currentTry-1][c].letter,word.charAt(c),letter[currentTry-1][c].color);
+    }
+    currentTry++;
+  }
+}
+
+function findWord(line){//outputs the word at a line
+  let w = "";
+  for(let i= 0;i<numberOfLetters;i++){
+    w = w+letter[line][i].letter
+  }
+  return w;
+}
+
 function keyPressed(){
   if(gameStatus == "playing"){
     typing();
@@ -151,18 +177,12 @@ function typing(){
     findEmptySpot();
   }
 }
-
-function checkWord(){
-  if(letters[currentTry-1][numberOfLetters-1] != "empty"){
-    currentTry++;
-  }
-}
   
 function findEmptySpot(){
   for(let r = 0;r<currentTry;r++){
     for(let c = 0;c<numberOfLetters;c++){
-      if(letters[r][c] == "empty"){
-        letters[r][c] = key;
+      if(letter[r][c].letter == "empty"){
+        letter[r][c].letter = key;
         return;
       }
     }
@@ -175,7 +195,7 @@ function deleteLastLetter(){
   let somethingToDelete = false;
   for(let r = 0;r<numberOfTries;r++){
     for(let c = 0;c<numberOfLetters;c++){
-      if(letters[r][c] != "empty" && r>=currentTry-1){
+      if(letter[r][c].letter != "empty" && r>=currentTry-1){
         placeToDeleteR = r;
         placeToDeleteC = c;
         somethingToDelete = true;
@@ -183,6 +203,6 @@ function deleteLastLetter(){
     }
   }
   if(somethingToDelete){
-    letters[placeToDeleteR][placeToDeleteC] = "empty";
+    letter[placeToDeleteR][placeToDeleteC].letter = "empty";
   }
 }
